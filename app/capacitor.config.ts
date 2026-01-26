0d9e49281488f72d8c18693d9d42f254e06460b4
Sam Watson <sammartinwatson@live.com>
2026-01-26 18:28:13 +0000

    Rename to Wickham Waves

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.wwuwh.app',
-  appName: 'DeepEvnts',
+  appName: 'Wickham Wave',
   webDir: 'dist',
   ios: {
     allowsLinkPreview: false,
